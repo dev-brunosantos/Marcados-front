f572b772542e5f41c9382167ef87318ea6685a5d
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-06-06 15:09:30 -0300

    Criando o modelo da tela HOME

---
+++ src/app/(pages)/home/page.tsx
@@ -0,0 +1,13 @@
+import { TeclaContainer, Tela } from "@/src/components/Tela";
+
+export default function Home() {
+    return (
+        <Tela>
+            <TeclaContainer>
+                <h1 className="text-3xl font-bold text-center">
+                    Bem-vindo ao Teste
+                </h1>
+            </TeclaContainer>
+        </Tela>
+    )
+}
